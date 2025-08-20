dedb5ae2e7fa04715888331d5a74756a25f64ac0
Panuwat Handsome <panuwat.c@ddlg-corp.com>
2025-08-20 16:51:41 +0700

    up

--- src/app/components/todocomponent/TodoItem.tsx
+++ src/app/components/todocomponent/TodoItem.tsx
@@ -1,17 +1,17 @@
-import { Button } from '@/components/ui/button'
-import { Card, CardContent } from '@/components/ui/card'
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 
 type Props = {
   todo: {
-    id: number
-    title: string
-    description?: string
-    completed: boolean
-  }
-  onEdit: (id: number, title: string, desc: string) => void
-  onDelete: (id: number) => void
-  onToggle: (id: number, completed: boolean) => void
-}
+    id: number;
+    title: string;
+    description?: string;
+    completed: boolean;
+  };
+  onEdit: (id: number, title: string, desc: string) => void;
+  onDelete: (id: number) => void;
+  onToggle: (id: number, completed: boolean) => void;
+};
 export default function TodoItem({ todo, onEdit, onDelete, onToggle }: Props) {
   return (
     <Card className="mb-2">
@@ -21,19 +21,31 @@ export default function TodoItem({ todo, onEdit, onDelete, onToggle }: Props) {
           <p className="text-sm break-all">{todo.description}</p>
         </div>
         <div className="flex flex-col sm:flex-row gap-2 w-full sm:w-auto">
-          <Button className={`cursor-pointer shadow-md ${todo.completed ? 'bg-yellow-500 text-white hover:bg-yellow-600' : 'bg-green-500 text-white hover:bg-green-600'}`} onClick={() => onToggle(todo.id, todo.completed)}>
+         <Button
+            className="cursor-pointer shadow-md"
+            variant={todo.completed ? 'secondary' : 'default'}
+            onClick={() => onToggle(todo.id, todo.completed)}
+          >
             {todo.completed ? 'Undo' : 'Done'}
           </Button>
 
-          <Button className="cursor-pointer shadow-md bg-red-500 text-white hover:bg-red-800" onClick={() => onDelete(todo.id)}>
+         <Button
+            className="cursor-pointer shadow-md"
+            variant="destructive"
+            onClick={() => onDelete(todo.id)}
+          >
             Delete
           </Button>
 
-          <Button className="cursor-pointer shadow-md bg-blue-500 text-white hover:bg-blue-600" onClick={() => onEdit(todo.id, todo.title, todo.description ?? '')}>
+               <Button
+            className="cursor-pointer shadow-md"
+            variant="outline"
+            onClick={() => onEdit(todo.id, todo.title, todo.description ?? '')}
+          >
             Edit
           </Button>
         </div>
       </CardContent>
     </Card>
-  )
+  );
 }
